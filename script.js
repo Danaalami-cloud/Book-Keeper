817e0e76d7c90ba9c588796c2781f222ab3de7c9
Dana Alami <dana.alami18@gmail.com>
2023-06-22 15:29:22 +0100

    fixing preventDefault issue

---
+++ script.js
@@ -0,0 +1,45 @@
+const modal = document.getElementById('modal');
+const modalShow = document.getElementById('show-modal');
+const modalClose = document.getElementById('close-modal');
+const bookmarkForm = document.getElementById('bookmark-form');
+const websiteNameEL = document.getElementById('website-name');
+const websiteUrlEl = document.getElementById('website-url');
+const bookmarksContainer = document.getElementById('bookmarks-container');
+
+// Show modal, Focus on the 1st input in the modal 
+function showModal() {
+    modal.classList.add('show-modal');
+    websiteNameEL.focus();
+}
+
+// Modal Event Listeners
+modalShow.addEventListener('click', showModal);
+modalClose.addEventListener('click', () => modal.classList.remove('show-modal'));
+window.addEventListener('click', (e) => (e.target === modal ? modal.classList.remove('show-modal') : false));
+
+// Validate form
+function validate(nameValue, urlValue) {
+    const expression = /(https)?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/g;
+    const regex = new RegExp(expression);
+    if(nameValue.match(regex)) {
+        alert('match');
+    }
+    if (!urlValue.match(regex)) {
+        alert('Please provide a valid web address');
+        return false;
+    }
+}
+
+// Handle Data from From
+function storeBookmark() {
+    e.preventDefault();
+    const nameValue = websiteNameEL.value;
+    let urlValue = websiteUrlEl.value;
+    if (!urlValue.includes('http://', 'https://')) {
+        urlValue = `https://${urlValue}`;
+    }
+    validate(nameValue, urlValue);
+}
+
+// Event Listener
+bookmarkForm.addEventListener('submit', storeBookmark);
